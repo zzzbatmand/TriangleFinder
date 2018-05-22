0eb823b77dd6777848d5d2b278ca446c8aa235bc
lars8053 <lars8053@LARS-PC>
2018-05-22 11:53:18 +0200

    Added files to git project.

---
+++ TriangleFinder/TriangleFinder/Form1.Designer.cs
@@ -0,0 +1,303 @@
+﻿namespace TriangleFinder
+{
+    partial class TriangleFinder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TriangleFinder));
+            this.methodLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.methodBox = new System.Windows.Forms.TextBox();
+            this.resultBox = new System.Windows.Forms.TextBox();
+            this.refImage = new System.Windows.Forms.PictureBox();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.submitButton = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.TextBox();
+            this.b2 = new System.Windows.Forms.TextBox();
+            this.a2 = new System.Windows.Forms.TextBox();
+            this.c1 = new System.Windows.Forms.TextBox();
+            this.b1 = new System.Windows.Forms.TextBox();
+            this.a1 = new System.Windows.Forms.TextBox();
+            this.background = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.refImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // methodLabel
+            // 
+            this.methodLabel.AutoSize = true;
+            this.methodLabel.Location = new System.Drawing.Point(177, 8);
+            this.methodLabel.Name = "methodLabel";
+            this.methodLabel.Size = new System.Drawing.Size(97, 13);
+            this.methodLabel.TabIndex = 55;
+            this.methodLabel.Text = "Calculation method";
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(12, 8);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(85, 13);
+            this.resultLabel.TabIndex = 54;
+            this.resultLabel.Text = "Calculated result";
+            // 
+            // methodBox
+            // 
+            this.methodBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.methodBox.Location = new System.Drawing.Point(180, 26);
+            this.methodBox.Multiline = true;
+            this.methodBox.Name = "methodBox";
+            this.methodBox.ReadOnly = true;
+            this.methodBox.Size = new System.Drawing.Size(240, 79);
+            this.methodBox.TabIndex = 53;
+            // 
+            // resultBox
+            // 
+            this.resultBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultBox.Location = new System.Drawing.Point(17, 27);
+            this.resultBox.Multiline = true;
+            this.resultBox.Name = "resultBox";
+            this.resultBox.ReadOnly = true;
+            this.resultBox.Size = new System.Drawing.Size(158, 78);
+            this.resultBox.TabIndex = 52;
+            this.resultBox.Text = "Find triangle info";
+            // 
+            // refImage
+            // 
+            this.refImage.Image = ((System.Drawing.Image)(resources.GetObject("refImage.Image")));
+            this.refImage.Location = new System.Drawing.Point(429, 23);
+            this.refImage.Name = "refImage";
+            this.refImage.Size = new System.Drawing.Size(188, 141);
+            this.refImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.refImage.TabIndex = 51;
+            this.refImage.TabStop = false;
+            // 
+            // resetButton
+            // 
+            this.resetButton.Location = new System.Drawing.Point(348, 113);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(75, 23);
+            this.resetButton.TabIndex = 50;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox6.Location = new System.Drawing.Point(302, 115);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(40, 20);
+            this.textBox6.TabIndex = 49;
+            this.textBox6.Text = "    C";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(244, 115);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(52, 20);
+            this.textBox5.TabIndex = 48;
+            this.textBox5.Text = "       B";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(186, 115);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(52, 20);
+            this.textBox4.TabIndex = 47;
+            this.textBox4.Text = "       A";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(128, 115);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(52, 20);
+            this.textBox3.TabIndex = 46;
+            this.textBox3.Text = "       c";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 115);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(52, 20);
+            this.textBox2.TabIndex = 45;
+            this.textBox2.Text = "       b";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 115);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(52, 20);
+            this.textBox1.TabIndex = 44;
+            this.textBox1.Text = "       a";
+            // 
+            // submitButton
+            // 
+            this.submitButton.Location = new System.Drawing.Point(348, 141);
+            this.submitButton.Name = "submitButton";
+            this.submitButton.Size = new System.Drawing.Size(75, 23);
+            this.submitButton.TabIndex = 43;
+            this.submitButton.Text = "Enter";
+            this.submitButton.UseVisualStyleBackColor = true;
+            this.submitButton.Click += new System.EventHandler(this.submitButton_Click);
+            // 
+            // c2
+            // 
+            this.c2.Location = new System.Drawing.Point(302, 143);
+            this.c2.Name = "c2";
+            this.c2.ReadOnly = true;
+            this.c2.Size = new System.Drawing.Size(40, 20);
+            this.c2.TabIndex = 42;
+            this.c2.Text = "90";
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(244, 143);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(52, 20);
+            this.b2.TabIndex = 41;
+            this.b2.Text = "0";
+            this.b2.TextChanged += new System.EventHandler(this.global_TextChanged);
+            // 
+            // a2
+            // 
+            this.a2.Location = new System.Drawing.Point(186, 143);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(52, 20);
+            this.a2.TabIndex = 40;
+            this.a2.Text = "0";
+            this.a2.TextChanged += new System.EventHandler(this.global_TextChanged);
+            // 
+            // c1
+            // 
+            this.c1.Location = new System.Drawing.Point(128, 143);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(52, 20);
+            this.c1.TabIndex = 39;
+            this.c1.Text = "0";
+            this.c1.TextChanged += new System.EventHandler(this.global_TextChanged);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(70, 143);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(52, 20);
+            this.b1.TabIndex = 38;
+            this.b1.Text = "0";
+            this.b1.TextChanged += new System.EventHandler(this.global_TextChanged);
+            // 
+            // a1
+            // 
+            this.a1.Location = new System.Drawing.Point(12, 143);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(52, 20);
+            this.a1.TabIndex = 37;
+            this.a1.Text = "0";
+            this.a1.TextChanged += new System.EventHandler(this.global_TextChanged);
+            // 
+            // background
+            // 
+            this.background.Location = new System.Drawing.Point(12, 23);
+            this.background.Multiline = true;
+            this.background.Name = "background";
+            this.background.ReadOnly = true;
+            this.background.Size = new System.Drawing.Size(411, 86);
+            this.background.TabIndex = 36;
+            // 
+            // TriangleFinder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(629, 176);
+            this.Controls.Add(this.methodLabel);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.methodBox);
+            this.Controls.Add(this.resultBox);
+            this.Controls.Add(this.refImage);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.submitButton);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.background);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "TriangleFinder";
+            this.Text = "Triangle Finder";
+            ((System.ComponentModel.ISupportInitialize)(this.refImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label methodLabel;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox methodBox;
+        private System.Windows.Forms.TextBox resultBox;
+        private System.Windows.Forms.PictureBox refImage;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button submitButton;
+        private System.Windows.Forms.TextBox c2;
+        private System.Windows.Forms.TextBox b2;
+        private System.Windows.Forms.TextBox a2;
+        private System.Windows.Forms.TextBox c1;
+        private System.Windows.Forms.TextBox b1;
+        private System.Windows.Forms.TextBox a1;
+        private System.Windows.Forms.TextBox background;
+    }
+}
+
---
+++ TriangleFinder/TriangleFinder/Form1.cs
@@ -0,0 +1,337 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TriangleFinder
+{
+    public partial class TriangleFinder : Form
+    {
+        private string lastChanged = "";
+        List<TextBox> textList = new List<TextBox>();
+        bool running = false;
+
+        #region Main_functions
+        public TriangleFinder()
+        {
+            InitializeComponent();
+            //Create list of textboxes
+            textList.Add(a1);
+            textList.Add(b1);
+            textList.Add(c1);
+            textList.Add(a2);
+            textList.Add(b2);
+        }
+
+        private void global_TextChanged(object sender, EventArgs e)
+        {
+            if (!running)
+            {
+                TextBox triggerBox = (sender as TextBox); //Get the triggered textbox.
+                Console.WriteLine(triggerBox.Name); //Print name of the triggered textbox.
+                lastChanged = lastChanged.Replace(triggerBox.Name, ""); //Remove old item, if triggered before.
+                if (triggerBox.Text != "0" && triggerBox.Text != "") //If the triggered box isn't 0 or empty.
+                {
+                    lastChanged += triggerBox.Name; //Add triggered box to list.
+                }
+                Console.WriteLine(lastChanged); //Print changed order.
+            }
+        }
+
+        private bool checkError()
+        {
+            if (lastChanged.Length < 4) //If there isn't 2 edited items.
+                return true;
+            /*if (a1.Text.Length == 0) { a1.Text = "0"; }
+            if (b1.Text.Length == 0) { b1.Text = "0"; }
+            if (c1.Text.Length == 0) { c1.Text = "0"; }
+            if (a2.Text.Length == 0) { a2.Text = "0"; }
+            if (b2.Text.Length == 0) { b2.Text = "0"; }*/
+            foreach (TextBox tb in textList)
+            {
+                try
+                {
+                    Convert.ToDouble(tb.Text);  //Check for conversion falure, used to see if input contaisn a character wich isn't a number.
+                }
+                catch (Exception)
+                {
+                    return true;
+                }
+
+                if (double.IsNaN(Convert.ToDouble(tb.Text))) //If any of the inputs is Nor a Number.
+                    return true;
+            }
+
+            return false;
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            resultBox.Text = "Find triangle info";
+            methodBox.Text = "";
+            foreach (TextBox tb in textList)
+            {
+                tb.Text = "0";
+            }
+        }
+
+        private void submitButton_Click(object sender, EventArgs e)
+        {
+            runCalculation();
+        }
+        #endregion
+
+        #region calculate_functions
+        private void prepareInputs()
+        {
+            foreach (TextBox tb in textList)
+            {
+                if (tb.Text.Contains("."))  //Check if used a dot instead of necessary comma.
+                    tb.Text = tb.Text.Replace(".", ",");
+                if (tb.Text.Contains(" "))  //Check and remove spaces.
+                    tb.Text = tb.Text.Replace(" ", String.Empty);
+                if (tb.Text.Length == 0)    //Make sure a textbox isn't empty. (Pure cosmetic)
+                    tb.Text = "0";
+            }
+        }
+
+        private string createOrder()
+        {
+            //Create order as int in what to calculate in.
+            //There are 5 inp. 2 is always used = 3.
+
+            string resultOrder = "";
+
+            switch (lastChanged.Substring(lastChanged.Length - 4)) //Take the last 4 characters.
+            {
+                case "a1c1": //a&c
+                case "c1a1":
+                    resultOrder = "ac";
+                    break;
+                case "b1c1": //b&c
+                case "c1b1":
+                    resultOrder = "bc";
+                    break;
+                case "a1b1": //a&b
+                case "b1a1":
+                    resultOrder = "ab";
+                    break;
+                case "c1a2": //c&A
+                case "a2c1":
+                    resultOrder = "cA";
+                    break;
+                case "c1b2": //c&B
+                case "b2c1":
+                    resultOrder = "cB"; //Use same order as above for 1&2
+                    break;
+                case "a1b2": //a&B
+                case "b2a1":
+                    resultOrder = "aB";
+                    break;
+                case "b1a2": //b&A
+                case "a2b1":
+                    resultOrder = "bA";
+                    break;
+                case "a1a2": //a&A
+                case "a2a1":
+                    resultOrder = "aA";
+                    break;
+                case "b1b2": //b&B
+                case "b2b1":
+                    resultOrder = "bB";
+                    break;
+
+                default:
+                    break;
+            }
+
+            return resultOrder;
+        }
+
+        private double[] calculate()
+        {
+            bool failed = false;
+            string order = createOrder();
+            string outputOrder = "1"; //Necessary since converting to double and forst int if 0 will be removed.
+            double a, b, c, A, B; // Declare variables
+            a = b = c = A = B = 0; // Set variables to 0;
+
+            //for (int i = 0; i < order.ToString().Length; i++)
+            foreach (char value in order) //Should set the neccesarry variables.
+            {
+                switch (value)
+                {
+                    case 'a':
+                        a = Convert.ToDouble(a1.Text);
+                        break;
+                    case 'b':
+                        b = Convert.ToDouble(b1.Text);
+                        break;
+                    case 'c':
+                        c = Convert.ToDouble(c1.Text);
+                        break;
+                    case 'd':
+                        A = Convert.ToDouble(a2.Text);
+                        break;
+                    case 'e':
+                        B = Convert.ToDouble(b2.Text);
+                        break;
+                    default:
+                        //Error
+                        failed = true;
+                        break;
+                }
+            }
+
+            // Calculations start here, if there happen to be no errors.
+            if (!failed)
+            {
+                // I take A and B first, to reduce possible calculation methods.
+                if (!order.Contains('A')) //If a isn't in the order, get that first.
+                {
+                    if (!order.Contains('B'))
+                    {
+                        if (!order.Contains('a'))// Sets A, remember * (180 / pi) to convert to degree.
+                        { A = Math.Acos(b / c) * (180 / Math.PI); outputOrder += "0"; }
+                        else if (!order.Contains('b'))
+                        { A = Math.Asin(a / c) * (180 / Math.PI); outputOrder += "1"; }
+                        else if (!order.Contains('c'))
+                        { A = Math.Atan(a / b) * (180 / Math.PI); outputOrder += "2"; }
+                    }
+                    else
+                    {
+                        A = 90 - B; // If B exists, then just use that.
+                        outputOrder += "3";
+                    }
+                }
+                if (!order.Contains('B')) // Just to make sure B is set, if A already exists.
+                {
+                    B = 90 - A;
+                    outputOrder += "4";
+                }
+                // Next i take the smaller ones here. (a-c)
+                if (!order.Contains('a')) // Take 'a' first and work from there.
+                {
+                    if (order.Contains('c'))
+                    { a = c * Math.Sin(A * Math.PI / 180); Console.WriteLine(a); outputOrder += "5"; }
+                    else // This means (b)
+                    { a = b * Math.Tan(A * Math.PI / 180); outputOrder += "6"; }
+                }
+                if (!order.Contains('b')) // If it doesn't have 'b', yada yada...
+                {
+                    if (order.Contains('c'))
+                    { b = c * Math.Cos(A * Math.PI / 180); outputOrder += "7"; }
+                    else // This means (a)
+                    { b = a * Math.Tan(B * Math.PI / 180); outputOrder += "8"; }
+                }
+                if (!order.Contains('c')) // Lastly we find 'c'
+                {
+                    c = Math.Sqrt((a * a) + (b * b)); // This is the only neccesarry way.
+                    outputOrder += "9";
+                }
+
+
+                // Reduce decimal digits to 3, but it only works with decimal and not doubles, so convert a bit.
+                a = Convert.ToDouble(Math.Truncate(Convert.ToDecimal(a) * 1000m) / 1000m);
+                b = Convert.ToDouble(Math.Truncate(Convert.ToDecimal(b) * 1000m) / 1000m);
+                c = Convert.ToDouble(Math.Truncate(Convert.ToDecimal(c) * 1000m) / 1000m);
+                A = Convert.ToDouble(Math.Truncate(Convert.ToDecimal(A) * 1000m) / 1000m);
+                B = Convert.ToDouble(Math.Truncate(Convert.ToDecimal(B) * 1000m) / 1000m);
+            }
+            else
+            {
+                MessageBox.Show("Opps. Something went wrong (calculate function)"); // Temp error message
+            }
+
+            // I'm just keeping the result as a variable, so it's not fixed.
+            double[] result = { a, b, c, A, B, Convert.ToDouble(outputOrder) };
+            return result; //First 2 outputs, then 3 unused inputs, for results.
+        }
+
+        private void printOutput(double[] results)
+        {
+            string output1 = ""; // Calc method using given values.
+            string output2 = ""; // Calc methid using default letters.
+            string inputs = results[5].ToString().Substring(1); //Removes the base int used for keeping first 0.
+            Console.WriteLine("Inputs is : " + inputs);
+            // Get inputs and use ifstatements to print order
+            // If not contain then show how to calc.
+            foreach (char value in inputs)
+            {
+                switch (value)
+                {
+                    case ('0'):
+                        output1 += String.Format("A = cos^-1({0}/{1})\r\n", results[1], results[2]); // 1 = b & 2 = c
+                        output2 += String.Format("A = cos^-1(b/c)\r\n");
+                        break;
+                    case ('1'):
+                        output1 += String.Format("A = sin^-1({0}/{1})\r\n", results[0], results[2]); // 0 = a & 2 = c
+                        output2 += String.Format("A = sin^-1(a/c)\r\n");
+                        break;
+                    case ('2'):
+                        output1 += String.Format("A = tan^-1({0}/{1})\r\n", results[0], results[1]); // 0 = a & 1 = b
+                        output2 += String.Format("A = tan^-1(a/b)\r\n");
+                        break;
+                    case ('3'):
+                        output1 += String.Format("A = 90 - {0}\r\n", results[4]); // 4 = B
+                        output2 += String.Format("A = 90(C) - B\r\n");
+                        break;
+                    case ('4'):
+                        output1 += String.Format("B = 90 - {0}\r\n", results[3]); // 3 = A
+                        output2 += String.Format("B = 90(C) - A\r\n");
+                        break;
+                    case ('5'):
+                        output1 += String.Format("a = {0} * sin({1})\r\n", results[2], results[3]); // 2 = c & 3 = A
+                        output2 += String.Format("a = c * sin(A)\r\n");
+                        break;
+                    case ('6'):
+                        output1 += String.Format("a = {0} * tan({1})\r\n", results[1], results[3]); // 1 = b & 3 = A
+                        output2 += String.Format("a = b * tan(A)\r\n");
+                        break;
+                    case ('7'):
+                        output1 += String.Format("b = {0} * cos({1})\r\n", results[2], results[3]); // 2 = c & 3 = A
+                        output2 += String.Format("b = c * cos(A)\r\n");
+                        break;
+                    case ('8'):
+                        output1 += String.Format("b = {0} * tan({1})\r\n", results[0], results[4]); // 0 = a & 4 = B
+                        output2 += String.Format("b = a * tan(B)\r\n");
+                        break;
+                    case ('9'):
+                        output1 += String.Format("c = sqrt({0}^2 + {1}^2)\r\n", results[0], results[1]); // 0 = a & 1 = b
+                        output2 += String.Format("c = sqrt(a^2 + b^2)\r\n");
+                        break;
+                }
+            }
+
+            resultBox.Text = output1;
+            methodBox.Text = output2;
+            foreach (TextBox tb in textList)
+            {
+                tb.Text = results[textList.IndexOf(tb)].ToString();
+            }
+        }
+
+        private void runCalculation()
+        {
+            running = true;
+            prepareInputs();
+            if (checkError())
+            {
+                MessageBox.Show("There eather isn't enough inputs, or an input doesn't contain a number.", "An error occured");
+                running = false;
+            }
+            else
+            {
+                double[] results = calculate();
+                printOutput(results);
+            }
+            running = false;
+        }
+        #endregion
+    }
+}
